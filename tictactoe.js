78b1502848cff3df161c50b21f5036ee76c22105
cis101 <cis101@secs.iub.edu.bd>
2017-02-07 19:30:40 -0800

    tictac toe

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,20 @@
 //JS code goes here
+var turn= true;
+var clicked =[];
+function play(event){ 
+    var element=event.target;
+
+    if (!clicked[element.id]) {
+	    clicked[element.id]=true;
+	    console.log(clicked);
+	    console.log(event);
+	    element.innerHTML= turn ? "X": "O";
+	    turn=!turn;
+	}
+
+	
+
+
+
+}
+
